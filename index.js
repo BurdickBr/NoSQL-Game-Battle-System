23ae3e280a59398640ba4b04816ed69cae4856b1
Daniel Lungaro <chunk_77@hotmail.com>
2021-04-18 14:07:42 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const { MongoClient } = require("mongodb");
+
+// Replace the uri string with your MongoDB deployment's connection string.
+const uri =
+  "mongodb+srv://GameBattleAdmin:GBAdminTest@nosqlgamebattlesystem.6mkqb.mongodb.net/test";
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db('sample_mflix');
+    const movies = database.collection('movies');
+
+    const newMovie = { 
+        title: "New Movie Test",
+        genres: ["Adventure", "Comedy"],           
+    }
+
+    movies.insertOne(newMovie);
+
+    // Query for a movie that has the title 'Back to the Future'
+    let query = { title: 'New Movie Test' };
+    let movie = await movies.findOne(query);
+
+    console.log(movie);
+
+    let randString = ' more stuff'
+
+    let movieUpdate = {
+        $set: {
+            title: "New Movie Test 2" + randString
+        }
+    };
+
+    movies.updateOne(query, movieUpdate);
+
+    query = { title: 'New Movie Test 2' + randString };
+    movie = await movies.findOne(query);
+    
+
+    console.log(movie);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
